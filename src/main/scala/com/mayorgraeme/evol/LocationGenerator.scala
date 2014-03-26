a795751a9022fd6beb7bbd097099d4906f1b720c
graeme <graeme@millie>
2014-03-26 21:20:25 +0000

    LocationUpdate generation

--- src/main/scala/com/mayorgraeme/evol/LocationGenerator.scala
+++ src/main/scala/com/mayorgraeme/evol/LocationGenerator.scala
@@ -8,33 +8,37 @@ class LocationGenerator(x: Int, y:Int) {
   val r = new Random
   
   //Forest Config
-  val minNoOfForrests = 4
-  val maxNoOfForrests = 15
+  val minNoOfForrests = 20
+  val maxNoOfForrests = 25
   val forrestMinRadius = 2
-  val forrestMaxRadius = 10
-  val percentChanceTree = 65
+  val forrestMaxRadius = 4
+  val percentChanceTree = 100
 
   //River Config
-  val minNoOfRivers = 0
-  val maxNoOfRivers = 1
+  val minNoOfRivers = 2
+  val maxNoOfRivers = 3
   val percentChanceChangeDirection = 40
   
   val map = Array.fill[Char](x,y)(' ')
   
   //Algorithm
-  val noOfForrests = getInBetween(forrestMinRadius, forrestMaxRadius)
-  for(x <- 0 to noOfForrests){
-    addAForrest
+  val noOfForrests = getInBetween(minNoOfForrests, maxNoOfForrests)
+  if(noOfForrests > 0){
+    for(x <- 0 to noOfForrests){
+      addAForrest
+    }
   }
   val noOfRivers = getInBetween(minNoOfRivers, maxNoOfRivers)
-  for(x <- 0 to noOfRivers){
-    addARiver
+  if(noOfRivers > 0){
+    for(x <- 0 to noOfRivers){
+      addARiver
+    }
   }
   
   def getInBetween(min: Int, max:Int): Int = {
     val dif = max - min;
     if(dif > 0){
-      r.nextInt(max - min) + min;
+      r.nextInt(max - min) + min - 1 ;
     }else{
       min
     }
@@ -45,7 +49,6 @@ class LocationGenerator(x: Int, y:Int) {
   def addAForrest = {
     val centreX = r.nextInt(x);
     val centreY = r.nextInt(y);
-    
     val size = getInBetween(forrestMinRadius, forrestMaxRadius)
     circleMembers(x, y, centreX, centreY, size){ (curX, curY, distance) => {
         if(r.nextInt(100) <= percentChanceTree){

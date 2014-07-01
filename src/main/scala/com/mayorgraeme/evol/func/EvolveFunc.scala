cef30f1102fb5ff52e3e262465534816db150810
Graeme-Miller <graemewmiller@btinternet.com>
2014-07-01 10:38:19 +0100

    Rest system

--- src/main/scala/com/mayorgraeme/evol/func/EvolveFunc.scala
+++ src/main/scala/com/mayorgraeme/evol/func/EvolveFunc.scala
@@ -4,21 +4,26 @@ package com.mayorgraeme.evol.func
 import com.mayorgraeme.evol.util.ArrayFunctions._
 import com.mayorgraeme.evol.LocationGenerator
 import com.mayorgraeme.evol._
+import com.mayorgraeme.evol.data.java.LocationData
+import com.mayorgraeme.evol.data.java.SystemInfo
+import com.mayorgraeme.evol.data.java.ActorData
 import com.mayorgraeme.evol.enums.LocationType._
+import java.util.ArrayList
+import java.util.UUID
 import scala.util.Random
 
 object EvolveFunc {
   
   val maxX = 25
   val maxY = 25
-  val startInhabitants = 4
+  val startInhabitants = 10
   val SEED_SPROUT_TIME = 2
   val MAX_AGE = 6
   
   val rand = new Random()
   def percentChance(percent: Int) = rand.nextInt(100) < percent
   
-  case class LocationInformation(val locationType: LocationType, x:Int, y:Int, inhabitants: Set[Inhabitant])
+  case class LocationInformation(val locationType: LocationType, uuid:Long, x:Int, y:Int, inhabitants: Set[Inhabitant])
   type World = Seq[Seq[LocationInformation]]
   
   val world: World = {
@@ -29,7 +34,7 @@ object EvolveFunc {
       val x = xEntry._2
       xEntry._1.zipWithIndex.map { yEntry =>
         val y = yEntry._2
-        new LocationInformation(yEntry._1, x, y, Set())
+        new LocationInformation(yEntry._1, UUID.randomUUID.getMostSignificantBits,  x, y, Set())
       }.toVector
     }.toVector
   }
@@ -40,7 +45,7 @@ object EvolveFunc {
     for(x <- Range(0, maxX)){
       newString = newString + "| "
       for(y <- Range(0, maxY)){
-        newString = newString + getLocationString(world(x)(y))
+        newString = newString + getLocationInformationChar(world(x)(y))
         newString = newString + " "
       }
       newString = newString + "|\n"
@@ -49,18 +54,22 @@ object EvolveFunc {
     println(newString)
   }
   
-  def getLocationString(locationInformation: LocationInformation):String = {
+  def getLocationChar(locationInformation: LocationInformation):Char = {
+    locationInformation.locationType match {
+      case WATER => '~'
+      case _ => ' '
+    }
+  }
+  
+  def getLocationInformationChar(locationInformation: LocationInformation):Char = {
         
     if(locationInformation.inhabitants.isEmpty){
-      locationInformation.locationType match {
-        case WATER => "~"
-        case _ => " "
-      }
+      getLocationChar(locationInformation)
     } else {            
       locationInformation.inhabitants.toSeq(rand.nextInt(locationInformation.inhabitants.size)) match {
-        case Animal() => "A"
-        case Seed(_, _) => "."
-        case Plant(_) => "*"
+        case Animal() => 'A'
+        case Seed(_, _) => '.'
+        case Plant(_) => '*'
       }
     }
   }
@@ -70,7 +79,7 @@ object EvolveFunc {
   def changeWorld(world: World, x: Int, y: Int, setChanger :Set[Inhabitant] => Set[Inhabitant]): World = {
     
     val oldInfo = world(x)(y)
-    val newInfo = new LocationInformation(oldInfo.locationType, oldInfo.x, oldInfo.y, setChanger(oldInfo.inhabitants))    
+    val newInfo = new LocationInformation(oldInfo.locationType, oldInfo.uuid, oldInfo.x, oldInfo.y, setChanger(oldInfo.inhabitants))    
     updateWorld(world, x, y, newInfo)
   }
   
@@ -93,6 +102,7 @@ object EvolveFunc {
   
   abstract class Inhabitant {
     def transformWorld(world: World, locationInformation: LocationInformation): World = world
+    def getActorData(): ActorData = null
   }
   case class Animal extends Inhabitant
   
@@ -113,7 +123,7 @@ object EvolveFunc {
   case class Plant(maxAge:Int) extends Inhabitant {
     val chanceOfPropogation = 50
     var currentAge = 0
-    val allowedLocationTypes = Set(SAND)
+    val allowedLocationTypes = Set(WATER)
     
     override def transformWorld(world: World, locationInformation: LocationInformation): World = {
       currentAge = currentAge + 1
@@ -123,7 +133,7 @@ object EvolveFunc {
         subFromWorld(world, locationInformation.x, locationInformation.y, this)
       } else if(percentChance(chanceOfPropogation)){
         //println("TREE TIME "+circleMembers[LocationInformation](world, locationInformation.x, locationInformation.y, 1).size)
-        val spacesWithoutPlants: Seq[LocationInformation] = circleMembers[LocationInformation](world, locationInformation.x, locationInformation.y, 1).filter{g => 
+        val spacesWithoutPlants: Seq[LocationInformation] = circleMembers[LocationInformation](world, locationInformation.x, locationInformation.y, 4).filter{g => 
           allowedLocationTypes.contains(g.locationType) && !g.equals(locationInformation) && g.inhabitants.forall{ g => 
             g match {
               case Plant(_) => false
@@ -144,21 +154,39 @@ object EvolveFunc {
     }
   }
   
+  def convertWorldToSystemInfo(world: World): SystemInfo= {
+    val array = Array.fill[java.util.List[ActorData]](maxX, maxY)(new ArrayList[ActorData]())
+    for (locationInformation <- world.flatten){
+      val x = locationInformation.x
+      val y = locationInformation.y
+      array(x)(y).add(new LocationData(getLocationChar(locationInformation), locationInformation.uuid))
+        
+      for(inhabitant <- locationInformation.inhabitants){
+        array(x)(y).add(inhabitant.getActorData)
+      }
+    }
+      
+    new SystemInfo(maxX, maxY, array)
+  }
   
+  
+  def transformWorld(worldParameter: World): World = {
+    val worldFlat: Seq[LocationInformation] = world.flatten
+    worldFlat.foldLeft(worldParameter){(world: World, locationInformation: LocationInformation) => {   
+        //println(locationInformation.inhabitants.size)
+        locationInformation.inhabitants.foldLeft(world){(world: World, inhabitant: Inhabitant) =>
+          inhabitant.transformWorld(world, locationInformation)
+        }
+      }
+    }
+  }
   
   def main(args: Array[String]): Unit = {
         
     var worldVar = fillWithRandom(world, Seed(SEED_SPROUT_TIME, MAX_AGE))
     while(true){
       Thread.sleep(1000)
-      val worldFlat: Seq[LocationInformation] = worldVar.flatten
-      worldVar = worldFlat.foldLeft(worldVar){(world: World, locationInformation: LocationInformation) => {   
-          //println(locationInformation.inhabitants.size)
-          locationInformation.inhabitants.foldLeft(world){(world: World, inhabitant: Inhabitant) =>
-            inhabitant.transformWorld(world, locationInformation)
-          }
-        }
-      }
+      worldVar = transformWorld(worldVar)
       
       printWorld(worldVar)
     }    
--- src/main/scala/com/mayorgraeme/evol/web/RestController.scala
+++ src/main/scala/com/mayorgraeme/evol/web/RestController.scala
@@ -3,6 +3,8 @@ package com.mayorgraeme.evol.web
 import javax.ws.rs.Path
 import akka.util.Timeout
 import com.mayorgraeme.evol.data.java.SystemInfo
+import com.mayorgraeme.evol.func.EvolveFunc
+import com.mayorgraeme.evol.func.EvolveFunc._
 import javax.ws.rs.GET
 import javax.ws.rs.core.MediaType
 import scala.concurrent.duration._
@@ -15,21 +17,40 @@ import scala.concurrent.Await
 
 @Path("evolve")
 class RestController {
-
-  @GET
-  @Produces(Array(MediaType.APPLICATION_JSON))
-  def processGet: SystemInfo = {    
-    println("processGet in")
-    implicit val timeout = Timeout(50 seconds);
-
-    val future = System.locationManagerActor?SystemInfoRequest
-    println("processGet OUT")
-    Await.result(future, timeout.duration) match {      
-      case SystemInfoResponse(x) => {println("processGet RESP"); x}
-      case _ => {println("RestController - unknown response"); new SystemInfo(0,0, null)}
+  
+  val lock: Object = new Object
+  
+  var world: World = EvolveFunc.world;
+  
+  def updateWorld(newWorld: World) = {
+    lock.synchronized {
+      world = newWorld
+    }
+  }
+  
+  def getWorld: World = {
+    lock.synchronized {
+      world
+    }
+  }
+  
+  class Updater extends Runnable {
+    def run(){
+      while(true){
+        Thread.sleep(1000)
+        val newWorld = transformWorld(getWorld)
+        updateWorld(newWorld)
+        printWorld(newWorld)
+      }      
     }
   }
   
   
-  
+  new Thread(new Updater).start
+
+  @GET
+  @Produces(Array(MediaType.APPLICATION_JSON))
+  def processGet: SystemInfo = {    
+    convertWorldToSystemInfo(getWorld)
+  }
 }
